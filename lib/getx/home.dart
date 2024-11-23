181007d04b4fd7c3ba2b883eeabb82314f5db441
rogliariping <rogliariping@gmail.com>
2024-11-23 13:04:49 +0000

    add home pages for provider, getx and cubit

---
+++ lib/getx/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomeGetX extends StatelessWidget {
+  int _counter = 0;
+
+  void _incrementCounter() {
+      _counter++;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("GetX Counter"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
